4b6ffceea8f012f0d46a6c4d4450cdec1b7fb3e4
praveenmahasena <praveenmahasena647@gmail.com>
2024-06-23 11:22:51 -0400

    com-3: adding features and more refactor

--- internal/listener/listen.go
+++ internal/listener/listen.go
@@ -1,1 +1,53 @@
 package listener
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+type Clients struct {
+	con []net.Conn
+}
+
+func Run() error {
+	li, liErr := net.Listen("tcp", ":42069")
+	if liErr != nil {
+		return liErr
+	}
+
+	for {
+		con, conErr := li.Accept()
+		clients := Clients{}
+		clients.con = append(clients.con, con)
+
+		if conErr != nil {
+			continue
+		}
+
+		messageCh := make(chan string)
+		go read(con, messageCh)
+		go write(clients.con, messageCh)
+	}
+	//return nil
+}
+
+func read(con net.Conn, ch chan<- string) {
+	s := bufio.NewScanner(con)
+
+	for s.Scan() {
+		fmt.Println(s.Text())
+		ch <- s.Text()
+	}
+}
+
+func write(con []net.Conn, ch <-chan string) {
+
+	for message := range ch {
+
+		for _, client := range con {
+			io.WriteString(client, message)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+
+	"github.com/praveenmahasena/server/internal/listener"
+)
+
 func main() {
+
+	if err := listener.Run(); err != nil {
+		log.Fatalln(err)
+	}
 
 }
