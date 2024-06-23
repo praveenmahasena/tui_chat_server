052ae015c72f0560dc2506c055340edb56045029
praveenmahasena <praveenmahasena647@gmail.com>
2024-06-23 01:36:33 -0400

    COM-3 refactor

--- internal/app.go
+++ internal/app.go
@@ -1,10 +1,1 @@
 package internal
-
-import (
-	"github.com/praveenmahasena/server/internal/listener"
-)
-
-func Start() error {
-	l := listener.New("tcp", ":42069")
-	return l.Listen()
-}
--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/praveenmahasena/server/internal"
-)
-
 func main() {
-	if err := internal.Start(); err != nil {
-		log.Fatalln(err)
-	}
+
 }
